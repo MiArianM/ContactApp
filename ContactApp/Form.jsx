a8f1f017726b1ea766a2739112798e50f8b1638a
Arian Pourhossein <Cod16duty@gmail.com>
2024-04-18 14:21:53 +0330

    Form Has Been Created !

---
+++ ContactApp/Form.jsx
@@ -0,0 +1,16 @@
+import styles from "./Form.module.css";
+function Form() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" , marginTop:"20px"}}>
+      <form className={styles.container}>
+        <input type="text" placeholder="Write Your First Name . ."/>
+        <input type="text" placeholder="Write Your Last Name . ."/>
+        <input type="text" placeholder="Your Email Here !"/>
+        <input type="number" placeholder="Your Phone Number Here !"/>
+        <button>Add Contact</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
