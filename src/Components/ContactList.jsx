bb21aa7193ffd4d95e3439b5a5abfdd88f83e910
Arian Pourhossein <Cod16duty@gmail.com>
2024-04-20 17:20:47 +0330

    Splitting Works Up into Another Component for Creating Information Lists

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,38 +1,15 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import liststyles from "../Styles/ContactList.module.css";
+import ListCreating from "./ListCreating";
 function ContactList({ UserData }) {
-  const deletehandler = (event) => {
-    const li = event.target.parentElement.parentElement.parentElement;
-    li.style.display = "none";
-  };
   return (
     <>
       <ul>
         {UserData.length ? (
-          UserData.map((contact) => (
-            <li key={contact.id}>
-              <div>
-                <i className="fa-brands fa-fantasy-flight-games"></i>
-                <h4>
-                  {contact.fname} {contact.lname}
-                </h4>
-              </div>
-              <div>
-                <i className="fa-solid fa-envelope"></i>
-                <h4>{contact.email}</h4>
-              </div>
-              <div>
-                <i className="fa-solid fa-square-phone"></i>
-                <h4>{contact.number}</h4>
-              </div>
-              <div>
-                <button onClick={deletehandler}>
-                  <i className="fa-solid fa-trash-can"></i>
-                </button>
-              </div>
-            </li>
-          ))
+          UserData.map((contact) => {
+            return <ListCreating key={contact.id} userinfo={contact} />;
+          })
         ) : (
           <h2>No Contact </h2>
         )}
---
+++ src/Components/ListCreating.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+function ListCreating({ userinfo }) {
+  return (
+    <li key={userinfo.id}>
+      <div>
+        <i className="fa-brands fa-fantasy-flight-games"></i>
+        <h4>
+          {userinfo.fname} {userinfo.lname}
+        </h4>
+      </div>
+      <div>
+        <i className="fa-solid fa-envelope"></i>
+        <h4>{userinfo.email}</h4>
+      </div>
+      <div>
+        <i className="fa-solid fa-square-phone"></i>
+        <h4>{userinfo.number}</h4>
+      </div>
+      <div>
+        <button>
+          <i className="fa-solid fa-trash-can"></i>
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default ListCreating;
